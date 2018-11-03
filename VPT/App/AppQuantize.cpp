324bd7bae2aacaeb55ee3e5a025cb1de7ee45e6e
wang <wanghk00a1@outlook.com>
2018-11-03 20:41:26 +0800

    finish quantize and dequentize

--- VPT/App/AppQuantize.cpp
+++ VPT/App/AppQuantize.cpp
@@ -81,9 +81,24 @@ void CAppQuantize::Dequantize555(unsigned char *quantizedImageData, unsigned cha
 unsigned char *CAppQuantize::Quantize565(int &qDataSize) {
 
 	// You can modify anything within this function, but you cannot change the function prototype.
+	int i, j;
+	unsigned int r, g, b;
+	unsigned short rgb16;
 
-	qDataSize = width * height * 2 ;
-	unsigned char *quantizedImageData = new unsigned char[width * height * 2] ;
+	qDataSize = width * height * 2;
+
+	unsigned char *quantizedImageData = new unsigned char[width * height * 2];
+
+	for (j = 0; j < height; j++) {
+		for (i = 0; i < width; i++) {
+			b = pInput[(i + j * width) * 3 + 0];	// Blue Color Component
+			g = pInput[(i + j * width) * 3 + 1];	// Red Color Component
+			r = pInput[(i + j * width) * 3 + 2];	// Green COlor Component
+			rgb16 = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3);
+			quantizedImageData[(i + j * width) * 2 + 0] = rgb16 & 0xFF;
+			quantizedImageData[(i + j * width) * 2 + 1] = (rgb16 >> 8) & 0xFF;
+		}
+	}
 
 	return quantizedImageData ;
 }
@@ -93,6 +108,21 @@ unsigned char *CAppQuantize::Quantize565(int &qDataSize) {
 void CAppQuantize::Dequantize565(unsigned char *quantizedImageData, unsigned char *unquantizedImageData) {
 
 	// You can modify anything within this function, but you cannot change the function prototype.
+	int i, j;
+	unsigned int r, g, b;
+	unsigned short rgb16;
+
+	for (j = 0; j < height; j++) {
+		for (i = 0; i < width; i++) {
+			rgb16 = quantizedImageData[(i + j * width) * 2 + 0] | (((unsigned short)quantizedImageData[(i + j * width) * 2 + 1]) << 8);
+			b = rgb16 & 0x1F;
+			g = (rgb16 >> 5) & 0x3F;
+			r = (rgb16 >> 11) & 0x1F;
+			unquantizedImageData[(i + j * width) * 3 + 0] = (b << 3);
+			unquantizedImageData[(i + j * width) * 3 + 1] = (g << 2);
+			unquantizedImageData[(i + j * width) * 3 + 2] = (r << 3);
+		}
+	}
 }
 
 void CAppQuantize::Process(void) {
@@ -112,6 +142,8 @@ void CAppQuantize::Process(void) {
 	Dequantize555(quantizedData, pOutput) ;
 
 	delete [] quantizedData ;
+
+	printf(_T("Original Size = %d, Quantized Size = %d, Compression Ratio = %2.2f\r\n"), width * height * 3, qDataSize, (double)width * height * 3 / qDataSize);
 
 	quantizedData = Quantize565(qDataSize) ;
 
